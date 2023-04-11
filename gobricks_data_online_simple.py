623bb91de3937cbed10447645fd9caf21ee309ed
Project0ne <40708415+Project0ne@users.noreply.github.com>
2023-04-11 23:31:06 +0800

    Update gobricks_data_online_simple.py

--- gobricks_data_online_simple.py
+++ gobricks_data_online_simple.py
@@ -7,15 +7,15 @@ url = 'https://raw.githubusercontent.com/Project0ne/gaozhuan_data/main/id_List.c
 df = pd.read_csv(url, header=None)
 ids = df[0].tolist()
 
+# Define the fieldnames for the CSV file
+fieldnames = ["id", "caption","price", "caption_en", "color_id",  "inventory" ]
+
 for id in ids:
     # URL to fetch data from
     url = f'https://gobricks.cn/frontend/v1/item/filter?product_id={id}&type=2&limit=96&offset=0'
 
     # Send a GET request to the URL and store the response
     response = requests.get(url)
-
-    # Define the fieldnames for the CSV file
-    fieldnames = ["id", "caption","price", "caption_en", "color_id",  "inventory" ]
 
     # Open a CSV file in append mode with utf-8 encoding and write the data to it
     with open("data.csv", "a", newline="", encoding="utf-8_sig") as csvfile:
@@ -24,4 +24,6 @@ for id in ids:
         if csvfile.tell() == 0:
             writer.writeheader()
         for data in response.json()['rows']:
-            writer.writerow(data)
+            # Extract only the fields that we want to write to the CSV file
+            filtered_data = {k: v for k, v in data.items() if k in fieldnames}
+            writer.writerow(filtered_data)

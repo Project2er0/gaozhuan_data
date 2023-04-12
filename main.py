d3d1d289e12044b2261e5f4f5561809795586962
Project0ne <40708415+Project0ne@users.noreply.github.com>
2023-04-13 00:03:46 +0800

    Update main.py

--- main.py
+++ main.py
@@ -1,41 +1,56 @@
+import urllib3
+urllib3.disable_warnings()
+
 import csv
 import pandas as pd
 import requests
-from github import Github
+import concurrent.futures
+import ssl
+from tqdm import tqdm
 
 # Read the ids from the id_List.csv file with utf-8 encoding
-url = 'https://gitee.com/Project0ne/gaozhuan_data/blob/main/id_List.csv'
+url = 'https://raw.githubusercontent.com/Project0ne/cdn-1/master/id_List.csv'
 df = pd.read_csv(url, header=None)
 ids = df[0].tolist()
 
 # Define the fieldnames for the CSV file
 fieldnames = ["id", "caption","price", "caption_en", "color_id",  "inventory" ]
 
-# Authenticate with the GitHub API using your personal access token
-g = Github("YOUR_PERSONAL_ACCESS_TOKEN")
-
-# Get the repository object
-repo = g.get_repo("Project0ne/gaozhuan_data")
-
-for id in ids:
+def fetch_data(id):
     # URL to fetch data from
     url = f'https://gobricks.cn/frontend/v1/item/filter?product_id={id}&type=2&limit=96&offset=0'
 
-    # Send a GET request to the URL and store the response
-    response = requests.get(url)
+    try:
+        # Send a GET request to the URL and store the response
+        response = requests.get(url, verify=False)
+    except requests.exceptions.ProxyError as e:
+        print("ProxyError occurred:", e)
+        return []
 
+    # Extract only the fields that we want to write to the CSV file
+    data = response.json()['rows']
+    filtered_data = [{k: v for k, v in d.items() if k in fieldnames} for d in data]
+    
+    # Update the progress bar
+    pbar.update(1)
+    
+    return filtered_data
+
+with concurrent.futures.ThreadPoolExecutor() as executor:
     # Open a CSV file in append mode with utf-8 encoding and write the data to it
     with open("data.csv", "a", newline="", encoding="utf-8_sig") as csvfile:
         writer = csv.DictWriter(csvfile, fieldnames=fieldnames)
         # Only write the header if the file is empty
         if csvfile.tell() == 0:
             writer.writeheader()
-        for data in response.json()['rows']:
-            # Extract only the fields that we want to write to the CSV file
-            filtered_data = {k: v for k, v in data.items() if k in fieldnames}
-            writer.writerow(filtered_data)
-
-# Commit the changes to the repository
-with open("data.csv", "r", encoding="utf-8_sig") as file:
-    content = file.read()
-    repo.create_file("data.csv", "Updated data.csv", content, branch="main")
+        
+        # Initialize the progress bar
+        pbar = tqdm(total=len(ids))
+        
+        # Submit the requests to the executor and write the data to the CSV file
+        for data in executor.map(fetch_data, ids):
+            for d in data:
+                writer.writerow(d)
+        
+        # Close the progress bar
+        pbar.close()

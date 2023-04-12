f4784aca56fa05d7b422a4da8d159bc1f5089430
Project0ne <40708415+Project0ne@users.noreply.github.com>
2023-04-12 12:07:32 +0800

    Update main.py

--- main.py
+++ main.py
@@ -1,14 +1,21 @@
 import csv
 import pandas as pd
 import requests
+from github import Github
 
 # Read the ids from the id_List.csv file with utf-8 encoding
-url = 'https://raw.githubusercontent.com/Project0ne/gaozhuan_data/main/id_List.csv'
+url = 'https://gitee.com/Project0ne/gaozhuan_data/blob/main/id_List.csv'
 df = pd.read_csv(url, header=None)
 ids = df[0].tolist()
 
 # Define the fieldnames for the CSV file
 fieldnames = ["id", "caption","price", "caption_en", "color_id",  "inventory" ]
+
+# Authenticate with the GitHub API using your personal access token
+g = Github("YOUR_PERSONAL_ACCESS_TOKEN")
+
+# Get the repository object
+repo = g.get_repo("Project0ne/gaozhuan_data")
 
 for id in ids:
     # URL to fetch data from
@@ -27,3 +34,8 @@ for id in ids:
             # Extract only the fields that we want to write to the CSV file
             filtered_data = {k: v for k, v in data.items() if k in fieldnames}
             writer.writerow(filtered_data)
+
+# Commit the changes to the repository
+with open("data.csv", "r", encoding="utf-8_sig") as file:
+    content = file.read()
+    repo.create_file("data.csv", "Updated data.csv", content, branch="main")
